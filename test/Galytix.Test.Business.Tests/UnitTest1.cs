9b1affe8603e40a7d3274028b10a767af5962b8a
Jan Đonny Záruba <dzonny.dz@gmail.com>
2023-05-19 18:38:40 +0200

    Started adding tests

---
+++ test/Galytix.Test.Business.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Galytix.Test.Business.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
